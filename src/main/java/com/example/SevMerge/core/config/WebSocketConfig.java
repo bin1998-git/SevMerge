dfcf84ecd4aeb7d7657f84fb055084804bd813aa
san <san1425865@gamil.com>
2026-05-31 15:17:29 +0900

    웹 소캣 config 구현중

---
+++ src/main/java/com/example/SevMerge/core/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.example.SevMerge.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
