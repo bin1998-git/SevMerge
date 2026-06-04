c4b17411b60ba11a45f4b143a4ad6423433dd748
dkswnsgus66 <dkswnsgus66@gmail.com>
2026-06-04 22:52:12 +0900

    portfolio재작업

--- src/main/java/com/example/SevMerge/portfolio/PortfolioRepository.java
+++ src/main/java/com/example/SevMerge/portfolio/PortfolioRepository.java
@@ -2,16 +2,22 @@ package com.example.SevMerge.portfolio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio,Long> {
 
     @Query("""
-        SELECT po FROM Portfolio po JOIN FETCH po.expertProfile WHERE po.expertProfile.member.id= :expertId AND isActive=true
+        SELECT po FROM Portfolio po JOIN FETCH po.expertProfile WHERE po.expertProfile.member.id= :expertId AND po.isActive=true
 """)
-    List<PortfolioResponse.ListDTO> findByExpertIdIsActive(Long expertId);
+    List<Portfolio> findByExpertIdIsActive(@Param("expertId") Long expertId);
 
+
+@Query("""
+    SELECT COUNT(po) FROM Portfolio po JOIN po.expertProfile WHERE po.expertProfile.member.id = :memberId AND po.isActive=true
+""")
+    Long countPortfolioByMemberId(@Param("memberId") Long memberId);
 }
 
 
