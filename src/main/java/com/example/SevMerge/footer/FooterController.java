2d5c93bec735fd932b6b856fd9c7c1b307f13c7a
dkswnsgus66 <dkswnsgus66@gmail.com>
2026-06-15 08:13:26 +0900

    faq작업

--- src/main/java/com/example/SevMerge/footer/FooterController.java
+++ src/main/java/com/example/SevMerge/footer/FooterController.java
@@ -1,16 +1,24 @@
 package com.example.SevMerge.footer;
 
 
+import com.example.SevMerge.core.util.Define;
+import com.example.SevMerge.member.Member;
+import com.example.SevMerge.member.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class FooterController {
 
+    private final MemberService memberService;
+
     // 고객센터
-    @GetMapping("/support")
+    @GetMapping("/supports")
     public String support(@RequestParam(required = false) String tab, Model model) {
         model.addAttribute("isSupport", tab == null);
         model.addAttribute("isAbout", "about".equals(tab));
@@ -19,28 +27,43 @@ public class FooterController {
         return "footerProc/support";
     }
     // 이용약관
-    @GetMapping("/policy/terms")
+    @GetMapping("/policys/terms")
     public String terms(Model model) {
         model.addAttribute("isTerms", true);
         return "footerProc/terms";
     }
     // 개인정보 방침
-    @GetMapping("/policy/privacy")
+    @GetMapping("/policys/privacy")
     public String privacy(Model model) {
         model.addAttribute("isPrivacy", true);
         return "footerProc/privacy";
     }
     // 광고방침
-    @GetMapping("/policy/ad")
+    @GetMapping("/policys/ad")
     public String ad(Model model) {
         model.addAttribute("isAd", true);
         return "footerProc/ad";
     }
     // 운영정책
-    @GetMapping("/policy/operation")
+    @GetMapping("/policys/operation")
     public String operation(Model model) {
         model.addAttribute("isOperation", true);
         return "footerProc/operation";
     }
 
+    // FAQ
+    @GetMapping("/policys/faq")
+    public String faq(Model model, HttpSession session){
+
+        Member sessionMember = (Member) session.getAttribute(Define.SESSION_USER);
+
+        Member memberEntity = memberService.findMemberById(sessionMember.getId());
+
+        memberEntity.isAdmin();
+
+        model.addAttribute("isAdmin",memberEntity);
+
+        return ""
+
+    }
 }
