0cc526ec1fbd44917250af392c75714cfb9b7d0e
san <san1425865@gamil.com>
2026-06-16 17:42:10 +0900

    feat: sse 알림 토스트 추가

---
+++ src/main/java/com/example/SevMerge/notification/NotificationSseService.java
@@ -0,0 +1,53 @@
+package com.example.SevMerge.notification;
+
+import com.example.SevMerge.member.Member;
+import com.example.SevMerge.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationSseService {
+
+    private final Map<Long, SseEmitter> emitterMap = new ConcurrentHashMap<>();
+
+    public SseEmitter createConnection(Long memberId) {
+        SseEmitter emitter = new SseEmitter(60 * 1000L);
+
+        emitterMap.put(memberId, emitter);
+
+        emitter.onCompletion(() -> emitterMap.remove(memberId, emitter));
+        emitter.onTimeout(() -> emitterMap.remove(memberId, emitter));
+        emitter.onError((e) -> emitterMap.remove(memberId, emitter));
+
+        try {
+            emitter.send(SseEmitter.event().name("connect").data("connected!"));
+        } catch (IOException e) {
+            emitterMap.remove(memberId);
+        }
+        return emitter;
+    }
+
+    public void sendNotification(Long receiverId, NotificationResponse.ListDTO resDTO) {
+        SseEmitter emitter = emitterMap.get(receiverId);
+        if (emitter == null) return;
+
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("notification")
+                    .data(resDTO, MediaType.APPLICATION_JSON)
+            );
+        } catch (IOException e) {
+            emitterMap.remove(receiverId);
+        }
+    }
+
+
+
+}
